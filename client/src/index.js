deec3f4bd73bf67557e79f97174b65f0c970f9f8
nicoWeb31 <nico.riot@free.fr>
2020-10-29 17:28:53 +0100

    store,redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDom from 'react-dom';
-import App from './component/App'
 
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+
+
+import App from './component/App';
+
+//creation du store
+const store = createStore(()=>[],{},applyMiddleware())
 
 ReactDom.render(
-    <App/>,
+    <Provider store={store}>
+    <App/>
+    </Provider>,
+
     document.querySelector('#root')
 );
