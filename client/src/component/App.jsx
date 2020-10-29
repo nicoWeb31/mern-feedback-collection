6c994a8a8f6b600976161b9691c2b8fc82515f06
nicoWeb31 <nico.riot@free.fr>
2020-10-29 19:25:15 +0100

    header and rooting

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+const Header = () => <h2>hearder</h2>;
+const DashBord = () => <h2>DashBord</h2>;
+const SureveilNew = () => <h2>SureveilNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+
 
 const App = () => {
     return (
         <div>
-            hi there !
+
+
+            <Header/>
+
+            <BrowserRouter>
+                <div>
+
+                    <Route path='/dash' component={DashBord}/>
+                    <Route exact={true} path='/serveys' component={DashBord}/>
+                    <Route path='/serveys/new' component={SureveilNew}/>
+
+                    <Route  exact path='/' component={Landing}/>
+                </div>
+
+            </BrowserRouter>
         </div>
     );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+//routing
+
+// const Header = () => <h2>hearder</h2>;
+// const DashBord = () => <h2>DashBord</h2>;
+// const SureveilNew = () => <h2>SureveilNew</h2>;
+// const Landing = () => <h2>Landing</h2>;
+// const App = () => {
+//     return (
+//         <div>
+
+
+//             <Header/>
+
+//             <BrowserRouter>
+//                 <div>
+
+//                     <Route path='/dash' component={DashBord}/>
+//                     <Route exact={true} path='/serveys' component={DashBord}/>
+//                     <Route path='/serveys/new' component={SureveilNew}/>
+
+//                     <Route  exact path='/' component={Landing}/>
+//                 </div>
+
+//             </BrowserRouter>
+//         </div>
+//     );
+// }
