82c7b97f9022bb9e66c0dd45f7871c65573b8eae
nicoWeb31 <nico.riot@free.fr>
2020-10-30 12:43:11 +0100

    landing page

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -3,11 +3,13 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header/Hearder';
 import { connect } from 'react-redux';
-import {fetchUser} from '../actions'
+import {fetchUser} from '../actions';
+
+import Landing from '../component/landing/Landing'
 
 const DashBord = () => <h2>DashBord</h2>;
 const SureveilNew = () => <h2>SureveilNew</h2>;
-const Landing = () => <h2>Landing</h2>;
+
 
 
 class App extends Component {
@@ -28,7 +30,6 @@ class App extends Component {
             <BrowserRouter>
                 <div className="container">
 
-                    <Route path='/' component={DashBord}/>
                     <Route exact path='/serveys' component={DashBord}/>
                     <Route path='/serveys/new' component={SureveilNew}/>
 
---
+++ client/src/component/landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './landing.style.css'
+
+
+const Landing = () => {
+    return (
+        <div className="_container">
+            <h1>Emaily !</h1>
+            Collect feedback from your users
+        </div>
+    );
+}
+
+export default Landing;
