07c4ea907a1a6c395cac488df4992b531216e977
nicoWeb31 <nico.riot@free.fr>
2020-11-01 18:55:09 +0100

    config for deploye in heroku

--- server.js
+++ server.js
@@ -51,6 +51,22 @@ billingRoute(app);
 //for heroku
 const PORT = process.env.PORT || 5002
 
+
+//config for production mode in heroku
+if(process.env.NODE_ENV === 'production'){
+    //express will serve up production asset
+    //like our main.js or maoin.css file
+    app.use(express.static('client/build'))
+
+
+    //express will serve up the index.html 
+    //if doesn't reconize the route
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 app.listen(PORT,()=>{
     console.log(`server run .....on port ${PORT}....;)`)
 });
