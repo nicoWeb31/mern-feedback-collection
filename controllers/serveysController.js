654257e692ca963a7a4f056555f9fda074fb9662
nicoWeb31 <nico.riot@free.fr>
2020-11-02 14:59:29 +0100

    end point post surveys and logic in controller

--- controllers/serveysController.js
+++ controllers/serveysController.js
@@ -1,5 +1,17 @@
+const mongoose = require('mongoose');
+
+const Servey = mongoose.model('surveys');
 
 exports.postServey = (req,res)=>{
 
+    const {title,body,subject,recipients} = req.body;
+    const newServey = new Servey({
+        title,
+        body,
+        subject,
+        recipients: recipients.split(',').map(email=>({email})), //map(email=>{return {email: email}})
+        _user: req.user._id,
+        sentDate: Date.now()  //methode de mongoose
+    })
 
 }
--- routes/serveysRoutes.js
+++ routes/serveysRoutes.js
@@ -1,12 +1,13 @@
 const serveysController = require('../controllers/serveysController')
 //middleware login , user is login ?
 const requireLogin = require('../middlewares/requireLogin')
+const requireCredit = require('../middlewares/requireCredits')
 
 
 module.exports = app => {
-    //user is login ? middleware
-    //check if user have any credrit midlleware 
-    app.post('/api/surveys', requireLogin, serveysController.postServey)
+    //user is login ? use middleware
+    //check if user have any credrit use midlleware 
+    app.post('/api/surveys', requireLogin,requireCredit, serveysController.postServey)
 
 
 
